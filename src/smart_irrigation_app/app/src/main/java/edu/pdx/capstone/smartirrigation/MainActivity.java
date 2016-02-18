e10a53353c751fc19ef6412c0b20221a6b3c0a60
Austin J. Morlan <morlan.austin@gmail.com>
2016-02-17 21:26:52 -0800

    App: Add basic layout with input fields

--- src/smart_irrigation_app/app/src/main/java/edu/pdx/capstone/smartirrigation/MainActivity.java
+++ src/smart_irrigation_app/app/src/main/java/edu/pdx/capstone/smartirrigation/MainActivity.java
@@ -17,15 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +39,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickButtonLatitudeGet(View view)
+    {
+
+    }
+
+    public void onClickButtonDateGet(View view)
+    {
+
     }
 }
