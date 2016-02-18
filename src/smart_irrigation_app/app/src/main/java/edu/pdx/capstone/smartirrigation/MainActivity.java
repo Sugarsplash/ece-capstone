6f60bd60293250a3957847841ec0dbe8aff273e8
Austin J. Morlan <morlan.austin@gmail.com>
2016-02-18 11:06:46 -0800

    App: Implement Get button for Latitude
    Latitude can be automatically retrieved if desired rather than entered manually.

--- src/smart_irrigation_app/app/src/main/java/edu/pdx/capstone/smartirrigation/MainActivity.java
+++ src/smart_irrigation_app/app/src/main/java/edu/pdx/capstone/smartirrigation/MainActivity.java
@@ -1,15 +1,29 @@
 package edu.pdx.capstone.smartirrigation;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener {
+
+    GoogleApiClient mGoogleApiClient = null;
+    Location mLastLocation;
+    final private int REQUEST_CODE_LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+
     }
 
     @Override
@@ -41,13 +66,81 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onConnected(Bundle connectionHint)
+    {
+        /* New permissions model for API 23 and beyond only */
+        int api_version = android.os.Build.VERSION.SDK_INT;
+        if (api_version >= Build.VERSION_CODES.M)
+        {
+            int hasLocationPermission = checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
+            if (hasLocationPermission != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_CODE_LOCATION_PERMISSION);
+                return;
+            }
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int param)
+    {
+        // Stub
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result)
+    {
+        // Stub
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
     public void onClickButtonLatitudeGet(View view)
     {
+        EditText input_latitude = (EditText) findViewById(R.id.input_latitude);
 
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null)
+        {
+            input_latitude.setText(String.valueOf(mLastLocation.getLatitude()));
+        }
+        else
+        {
+            input_latitude.setText(0);
+        }
     }
 
     public void onClickButtonDateGet(View view)
     {
 
+    }
+
+    /* Permissions for API 23 + */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case REQUEST_CODE_LOCATION_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED)
+                {
+                    Toast.makeText(MainActivity.this, "Access to Location Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
